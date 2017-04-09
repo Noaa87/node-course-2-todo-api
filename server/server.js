b3f71e4ba5b2b72f3dda8dacefaed8aad0f02e9a
simone borelli <simone borelli>
2017-04-09 17:30:38 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectID} = require('mongodb');
+
 var express = require('express');
 var bodyParse = require('body-parser');
 
@@ -30,6 +32,35 @@ app.get('/todos', (req, res) => {
             res.status(400).send(e);
     });
 });
+
+//Get /todos/1233455
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send('what the fuck');
+    }
+
+    Todo.findById(id).then((todo) => { 
+        if(!todo){
+            res.status(404).send('Id not found');
+        }else 
+            res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
+    //valid id using isValid
+        //404 - send back empty sned
+    
+    //findById
+        //success
+            //if todo - send it back
+            //if no todo - send back 404 with empty body
+        //error
+            //400 - and send empty body back
+});
+
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
